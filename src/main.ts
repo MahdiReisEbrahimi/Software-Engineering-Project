8ee61aebca68fc57c3faeba460c9ad13bda7351c
Mehdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-11-30 16:50:07 +0330

    Feat: A simple leaflet map with clustering and a temporary jeojson file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 
 import App from './App.vue'
 import router from './router'
--- src/views/map/Map.vue
+++ src/views/map/Map.vue
@@ -1,3 +1,36 @@
 <template>
-  hello map
+  <div id="map" class="h-2/3 m-2"></div>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet.markercluster'
+
+const markers = L.markerClusterGroup({
+  showCoverageOnHover: false,
+  maxClusterRadius: 40,
+})
+
+onMounted(() => {
+  const map = L.map('map').setView([35.6892, 51.389], 12)
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+  }).addTo(map)
+
+  fetch('/assets/lawyers.geojson')
+    .then((res) => res.json())
+    .then((data) => {
+      L.geoJSON(data, {
+        pointToLayer: (feature, latlng) => {
+          const marker = L.marker(latlng).bindPopup(`<b>${feature.properties.name}</b>`)
+          markers.addLayer(marker)
+          return marker
+        },
+      })
+
+      map.addLayer(markers)
+    })
+})
+</script>
