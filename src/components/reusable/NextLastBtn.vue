cb03b7cf001c22640cd5c89e1fc317e0d421a53e
Mehdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-12-15 14:37:25 +0330

    Feat: button click emmit added

--- src/components/reusable/NextLastBtn.vue
+++ src/components/reusable/NextLastBtn.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="flex items-center justify-center flex-col mx-1">
-    <button class="">
-      <AkCircleChevronLeftFill v-if="props.direction === 'left'" class="text-3xl" />
-      <AkCircleChevronRightFill v-if="props.direction === 'right'" class="text-3xl" />
+  <div class="flex items-center">
+    <button @click="handleClick" class="rounded-lg p-2 mx-1 mt-1 hover:bg-gray-600 duration-500">
+      <BsChevronLeft v-if="props.direction === 'left'" class="text-3xl text-white" />
+      <BsChevronRight v-if="props.direction === 'right'" class="text-3xl text-white" />
     </button>
   </div>
 </template>
 <script lang="ts" setup>
-import { AkCircleChevronLeftFill , AkCircleChevronRightFill } from '@kalimahapps/vue-icons'
-
+import { BsChevronLeft, BsChevronRight } from '@kalimahapps/vue-icons'
 const props = defineProps({
   direction: {
     required: true,
     type: String,
   },
 })
+
+const emit = defineEmits<{
+  (e: 'click', direction: string): void
+}>()
+
+const handleClick = () => {
+  emit('click', props.direction)
+}
 </script>
