bc4667042545cd854ad80269380b5510bf474305
Mehdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-12-16 14:50:28 +0330

    Feat: signup page added

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -4,10 +4,12 @@
     style="background-image: url('/assets/img/background.jpg')"
   >
     <div class="w-5/6 m-auto max-w-md bg-gray-800 p-8 rounded-2xl shadow-lg lg:mr-20">
-      <LoginForm />
+      <LoginForm v-if="!showSignup" />
+      <SignupForm v-if="showSignup" />
       <div class="flex items-center justify-center mt-3">
-        <button class="text-gray-400 text-xs">
-          <u>ثبت نام نکرده اید؟</u>
+        <button @click="toggleSignupLogin" class="text-gray-400 text-xs">
+          <u v-if="!showSignup">ثبت نام نکرده اید؟</u>
+          <u v-if="showSignup">از قبل ثبت نام کرده‌اید؟</u>
         </button>
       </div>
     </div>
@@ -15,5 +17,13 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import LoginForm from './components/LoginForm.vue'
+import SignupForm from './components/SignupForm.vue'
+
+const showSignup = ref(false)
+
+function toggleSignupLogin() {
+  showSignup.value = !showSignup.value
+}
 </script>
