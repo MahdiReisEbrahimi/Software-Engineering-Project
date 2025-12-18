0022c688d4d53d342fece812bd7cca236c85e486
Mehdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-12-18 11:58:52 +0330

    Feat: add label to reusable button

--- src/components/reusable/ReusableButton.vue
+++ src/components/reusable/ReusableButton.vue
@@ -2,11 +2,12 @@
   <button
     :type="props.type"
     :disabled="props.disabled"
-    class="relative inline-flex items-center justify-center px-4 py-2 rounded-lg font-medium transition-all duration-200text-gray-800 hover:text-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+    class="relative inline-flex items-center justify-center px-4 py-2 rounded-lg font-medium transition-all duration-200text-gray-800 disabled:opacity-50 disabled:cursor-not-allowed"
+    :class="props.underline ? 'text-white' : 'text-gray-400'"
     @click="emits('click')"
   >
-    <slot />
-    <span v-if="props.underline" class="absolute -bottom-1 left-0 h-0.5 w-full bg-current" />
+    {{ label }}
+    <span v-if="props.underline" class="absolute bottom-1 left-3 h-0.5 w-4/5 bg-current" />
   </button>
 </template>
 
@@ -15,6 +16,7 @@ const props = defineProps<{
   type?: 'button' | 'submit' | 'reset'
   underline?: boolean
   disabled?: boolean
+  label: string
 }>()
 
 const emits = defineEmits(['click'])
